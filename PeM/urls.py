b97a1b4c2f08172e83659eb75ac4043d7a030fed
PDJ6975 <antrodcal@alum.us.es>
2025-11-11 18:19:52 +0100

    feat: template base para el desarrollo de vistas

--- PeM/urls.py
+++ PeM/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from core import views
 
 # Personalización del admin para branding
 admin.site.site_header = "PeM - Administración"
@@ -24,4 +27,9 @@ admin.site.index_title = "Panel de Administración - Juguetes para Mascotas"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
+
+# Servir archivos media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Vista de la página de inicio"""
+    return render(request, 'core/index.html')
