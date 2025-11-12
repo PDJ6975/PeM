d116d35a9e63b0f0e2fb74ee665e4f57cb191be9
Adriram04 <adriramgil@gmail.com>
2025-11-12 18:57:18 +0100

    feat: definition of endpoint with filters.
     Refs: #26

--- PeM/urls.py
+++ PeM/urls.py
@@ -1,38 +1,13 @@
-"""
-URL configuration for PeM project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views
 
-# Personalización del admin para branding
-admin.site.site_header = "PeM - Administración"
-admin.site.site_title = "PeM Admin"
-admin.site.index_title = "Panel de Administración - Juguetes para Mascotas"
-
 urlpatterns = [
     # Admin
     path('admin/', admin.site.urls),
-
-    # Página principal
-    path('', views.home, name='home'),
-
-    # Autenticación de usuarios
+    path('', include('core.urls')),
     path('login/', views.login_page, name='login'),
     path('register/', views.register_page, name='register'),
     path('logout/', views.logout_view, name='logout'),
@@ -61,6 +36,6 @@ urlpatterns = [
     path('seguimiento/<uuid:tracking_token>/', views.SeguimientoPedidoView.as_view(), name='seguimiento_pedido'),
 ]
 
-# Servir archivos media en desarrollo
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
